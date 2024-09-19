223efe53b4dc1a49da414843028673164c91262e
FerikLS <68301856+FerikLS@users.noreply.github.com>
2024-09-19 10:35:30 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+      var responseText = "Hello World!<br>";
+      responseText += "<small>Requested at: " + req.requestTime + "</small>";
+      res.send(responseText);
+    });
+    
 
 app.post("/", (req, res) => {
       res.send("Got a POST request");
@@ -23,6 +26,30 @@ app.listen(3000, () => {
     console.log("Example app listening on port 3000!");
 });
 
+const myLogger = (req, res, next) => {
+      console.log("LOGGED");
+      next();
+    };
+    app.use(myLogger);
+
 app.use('/static', express.static(__dirname + '/public'));
 
+//app.use((req, res, next) => {
+  //    res.status(404).send("Sorry cant find that!");
+   // });
+    
+    app.use((err, req, res, next) => {
+          console.error(err.stack);
+          res.status(500).send("Something broke!");
+        });
 
+        var birds = require('./birds');
+
+app.use('/birds', birds);
+
+const requestTime = (req, res, next) => {
+      req.requestTime = Date.now();
+      next();
+    };
+    app.use(requestTime);
+    
